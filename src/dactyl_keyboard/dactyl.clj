99f3e87690243d32fd38a6bd9fed256480d95bc9
quentin <quentin.lebastard@hardis.fr>
2019-05-25 17:59:26 +0200

    Revert "make square hole bigger and centered"
    This reverts commit 48798b340ee4ae6cac8d4293f81aca7424a30f81.

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -612,7 +612,7 @@
 (def usb-holder-space  (translate (map + usb-holder-position [0 (* -1 wall-thickness) 1]) usb-holder-cube))
 (def usb-holder-holder (translate usb-holder-position (cube 19 12 4)))
 
-(def usb-jack (translate (map + usb-holder-position [-5 10 5]) (cube 22.1 27 7.1)))
+(def usb-jack (translate (map + usb-holder-position [-7 10 5]) (cube 20.1 25 7.1)))
 
 (def pro-micro-position (map + (key-position 0 1 (wall-locate3 -1 0)) [-6 10 -10]))
 (def pro-micro-space-size [4 10 10]) ; z has no wall;
@@ -765,6 +765,6 @@
 
 (spit "things/test.scad"
       (write-scad
-       (difference )))
+       (difference trrs-holder trrs-holder-hole)))
 
 (defn -main [dum] 1)  ; dummy to make it easier to batch
