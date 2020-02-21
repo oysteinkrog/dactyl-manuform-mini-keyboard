a367c123ccd9b57897ec78d44a03988e2d070880
Øystein Krog <oystein.krog@gmail.com>
2020-02-21 16:35:16 +0100

    tweak promicro position

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -616,7 +616,7 @@
 
 (def usb-jack (translate (map + usb-holder-position [0 10 4]) (cube 12.1 20 6)))
 
-(def pro-micro-position (map + (key-position 0 1 (wall-locate3 -1 0)) [-6 10 -10]))
+(def pro-micro-position (map + (key-position 0 1 (wall-locate3 -1 0)) [-3 15 -13]))
 (def pro-micro-space-size [4 10 10]) ; z has no wall;
 (def pro-micro-wall-thickness 3)
 (def pro-micro-holder-size [(+ pro-micro-wall-thickness (first pro-micro-space-size)) (+ pro-micro-wall-thickness (second pro-micro-space-size)) (last pro-micro-space-size)])
