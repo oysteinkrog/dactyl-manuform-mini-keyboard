dc174d7a53d6217a0c757323c3fb34cd5f745d1e
Øystein Krog <oystein.krog@gmail.com>
2020-04-08 21:41:06 +0200

    add rx,ry,rz and rdx,ry,rdz

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -41,6 +41,14 @@
 (defn ty [dy shape] (translate [0 dy 0] shape))
 (defn tz [dz shape] (translate [0 0 dz] shape))
 
+(defn rx [radians shape] (rotate radians [1 0 0] shape))
+(defn ry [radians shape] (rotate radians [0 1 0] shape))
+(defn rz [radians shape] (rotate radians [0 0 1] shape))
+
+(defn rdx [degrees shape] (rx (deg2rad degrees) shape))
+(defn rdy [degrees shape] (ry (deg2rad degrees) shape))
+(defn rdz [degrees shape] (rz (deg2rad degrees) shape))
+
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; Shape parameters ;;
 ;;;;;;;;;;;;;;;;;;;;;;
